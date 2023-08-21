a0118c63029f85eaa28a16755d69c4adc7ca8f99
Elias Solis <elias3847040@gmail.com>
2023-08-20 22:25:02 -0300

    Primer Commit

---
+++ FrontEnd/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+const routes: Routes = [
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path: 'header', component: HeaderComponent},
+  { path:'footer', component: FooterComponent},
+  {path:'home',component:HomeComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
